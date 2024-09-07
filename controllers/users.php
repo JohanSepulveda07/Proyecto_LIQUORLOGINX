ac581bfa06144c50058b56cc8e3b835b11b991cd
JohanSepulveda101 <Johan-sepulveda0510@hotmail.com>
2024-09-07 15:02:17 -0500

    4. Modelo de Conexión a la Base de Datos

---
+++ controllers/users.php
@@ -0,0 +1,38 @@
+<?php
+[2:42 p.m., 7/9/2024] lucho Olaya: $user3 = new User;
+$user3->setUserId(3);
+$user3 ->setUserName("John");
+$user3 ->setUserAddress("cra 50 #71-45");
+$user3 ->setUserCategory("A");
+$user3 ->setUserPhone("12345678910");
+$user3 ->setUserStatus("Activo");
+
+echo "Código: " . $user3->getUserId() . "<br>";            
+echo "Nombre: " . $user3->getUserName() . "<br>";
+echo "Dirección: " . $user3->getUserAddress() . "<br>";
+echo "Categoria: " . $user3->getUserCategory() . "<br>";
+echo "Telefono: " . $user3->getUserPhone() . "<br>";
+echo "Dirección: " . $user3->getUserAddress() . "<br>";
+echo "Estado: " . $user3->getUserStatus() . "<br>";
+
+
+
+[2:42 p.m., 7/9/2024] lucho Olaya: $user3 = new User;
+$user3->setUserId(3);
+$user3 ->setUserName("John");
+$user3 ->setUserAddress("cra 50 #71-45");
+$user3 ->setUserCategory("A");
+$user3 ->setUserPhone("12345678910");
+$user3 ->setUserStatus("Activo");
+
+echo "Código: " . $user3->getUserId() . "<br>";            
+echo "Nombre: " . $user3->getUserName() . "<br>";
+echo "Dirección: " . $user3->getUserAddress() . "<br>";
+echo "Categoria: " . $user3->getUserCategory() . "<br>";
+echo "Telefono: " . $user3->getUserPhone() . "<br>";
+echo "Dirección: " . $user3->getUserAddress() . "<br>";
+echo "Estado: " . $user3->getUserStatus() . "<br>";
+
+
+
+?>
